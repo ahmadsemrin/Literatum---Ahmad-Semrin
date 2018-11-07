2078529b66e04fc97a09509d74ee3894b8cd23cc
ahmadsemrin <ahmad.semrin@hotmail.com>
2018-11-08 01:10:30 +0200

    Lit-01.11.01: Small modifications.

--- src/main/java/webappdesign/action/LoginAction.java
+++ src/main/java/webappdesign/action/LoginAction.java
@@ -7,16 +7,29 @@ import java.sql.*;
 public class LoginAction {
     public void login(User user) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/Literatum","root","123");
 
-            String query = "INSERT INTO User(email, password) VALUES (?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, user.getEmail());
-            preparedStatement.setString(2, user.getPassword());
+            //String query = "INSERT INTO User(email, password) VALUES (?, ?)";
+            String query = "SELECT (email, password) FROM User WHERE email = " + user.getEmail();
+            Statement statement = connection.createStatement();
 
-            preparedStatement.execute();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            User user2 = null;
+            while (resultSet.next()) {
+                user2 = new User();
+
+                user2.setEmail(resultSet.getString("email"));
+                user2.setPassword(resultSet.getString("password"));
+            }
+
+            if (user.getEmail().equals(user2.getEmail())) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
 
             connection.close();
         } catch (ClassNotFoundException ex){
