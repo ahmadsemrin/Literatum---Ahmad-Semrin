7af9c86e4eff5db96e5eabfc99a07377ea90351f
asemrin <ahmad.semrin@hotmail.com>
2018-11-08 15:40:15 +0200

    Lit-01.12: User can login.

--- src/main/java/webappdesign/action/LoginAction.java
+++ src/main/java/webappdesign/action/LoginAction.java
@@ -12,23 +12,27 @@ public class LoginAction {
                     "jdbc:mysql://localhost:3306/Literatum","root","123");
 
             //String query = "INSERT INTO User(email, password) VALUES (?, ?)";
-            String query = "SELECT (email, password) FROM User WHERE email = " + user.getEmail();
-            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM User WHERE email = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user.getEmail());
 
-            ResultSet resultSet = statement.executeQuery(query);
-
-            User user2 = null;
-            while (resultSet.next()) {
-                user2 = new User();
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                User user2 = new User();
 
                 user2.setEmail(resultSet.getString("email"));
                 user2.setPassword(resultSet.getString("password"));
-            }
 
-            if (user.getEmail().equals(user2.getEmail())) {
-                System.out.println("yes");
+
+                if (user.getEmail().equals(user2.getEmail())) {
+                    System.out.println("yes");
+                    System.out.println(user.getEmail());
+                    System.out.println(user2.getEmail());
+                } else {
+                    System.out.println("no");
+                }
             } else {
-                System.out.println("no");
+                System.out.println("This user is not existed! Try to register to Atypon.");
             }
 
             connection.close();
